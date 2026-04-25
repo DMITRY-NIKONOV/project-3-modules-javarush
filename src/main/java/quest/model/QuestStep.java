4a5e8bd5df33b45357410051e349c372ae653c11
Dmitry Nikonov <nikonov9094@gmail.com>
2026-04-25 14:54:19 +0500

    add QuestStep, QuestService

---
+++ src/main/java/quest/model/QuestStep.java
@@ -0,0 +1,21 @@
+package quest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class QuestStep {
+
+    private String id;
+    private String text;
+    private String option1;
+    private String option2;
+    private String nextStepId1;
+    private String nextStepId2;
+
+}
